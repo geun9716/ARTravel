46811066b7cf3fc6331f9857fea4bbbe11b374f1
Taewan Go <33471826+solidw@users.noreply.github.com>
2021-02-05 04:39:56 +0900

    Initialize App (#5)

--- code-samples/index.ios.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App.js';
-
-// The below line is necessary for use with the TestBed App
-AppRegistry.registerComponent('ViroSample', () => App);
