27cc36c13ae141f6982c826917f3ce5d325799d4
geun9716 <crintop@gmail.com>
2021-02-06 03:23:02 +0900

    DB에서 받아온 데이터를 AR로 포지셔닝

--- ARTravel-app/src/ViroExamples/ARSample/HelloWorldSceneAR2.js
+++ ARTravel-app/src/ViroExamples/ARSample/HelloWorldSceneAR2.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react';
 import Axios from '../../modules/ApiClient';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import Geolocation from 'react-native-geolocation-service';
 import {
   ViroARScene,
@@ -15,6 +15,8 @@ import {
   ViroAnimations,
 } from 'react-viro';
 
+
+
 export default class HelloWorldSceneAR extends Component {
 
   constructor() {
@@ -26,11 +28,13 @@ export default class HelloWorldSceneAR extends Component {
       posts : [],
     };
 
+
     // bind 'this' to functions
-    // this._onInitialized = this._onInitialized.bind(this);
-    // this._latLongToMerc = this._latLongToMerc.bind(this);
-    // this._transformPointToAR = this._transformPointToAR.bind(this);
+    this._onInitialized = this._onInitialized.bind(this);
+    this._latLongToMerc = this._latLongToMerc.bind(this);
+    this._transformPointToAR = this._transformPointToAR.bind(this);
   }
+
 
   componentDidMount = () => {
     Geolocation.getCurrentPosition(
@@ -59,29 +63,44 @@ export default class HelloWorldSceneAR extends Component {
   }
 
   render() {
-    const {posts} = this.state;
-
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized} >
-
-        {/* <ViroText text={this.state.text} scale={[.2,2,.2]} position={[0, -2, -5]} style={styles.helloWorldTextStyle} />
-        <ViroText text="North Text" scale={[3, 3, 3]} transformBehaviors={["billboard"]} position={[this.state.northPointX, 0, this.state.northPointZ]} style={styles.helloWorldTextStyle} />
-        <ViroText text="South Text" scale={[3, 3, 3]} transformBehaviors={["billboard"]} position={[this.state.southPointX, 0, this.state.southPointZ]} style={styles.helloWorldTextStyle} />
-        <ViroText text="West Text" scale={[3, 3, 3]} transformBehaviors={["billboard"]} position={[this.state.westPointX, 0, this.state.westPointZ]} style={styles.helloWorldTextStyle} />
-        <ViroText text="East Text" scale={[3, 3, 3]} transformBehaviors={["billboard"]} position={[this.state.eastPointX, 0, this.state.eastPointZ]} style={styles.helloWorldTextStyle} /> */}
-
         <ViroAmbientLight color={"#aaaaaa"} influenceBitMask={1} />
 
-        
+        {this.state.posts.map((val, index) => {
+          let point = this._transformPointToAR(val.latitude, val.longitude);
+          console.log(val);
+          return (
+            <ViroNode
+              key = {index}
+              position={[point.x, 0, point.z]}
+              scale={[.15, .15, .15]}
+              animation={{name: "rotate", run: true, loop: true}}>
+              <Viro3DObject
+                source={require()}
+                type="OBJ"
+                resources={[require('../../assets/heart-t.mtl')]}
+                onClick={this._onClick}
+                materials={["blue_sphere"]}/>
+              <ViroQuad
+                rotation={[-90,0,0]}
+                width={.5} height={.5}
+                arShadowReceiver={true}
+                lightReceivingBitMask={4} />
+            </ViroNode>
+          );
+        })}
 
-        <ViroNode
+
+
+        {/* <ViroNode
           position={[0, 0, -1]}
           scale={[.15, .15, .15]}
           animation={{name: "rotate", run: true, loop: true}}>
           <Viro3DObject
-            source={require('../../assets/heart-t.obj')}
+            source={require('../../assets/bag-t.obj')}
             type="OBJ"
-            resources={[require('../../assets/heart-t.mtl')]}
+            resources={[require('../../assets/bag-t.mtl')]}
             onClick={this._onClick}
             materials={["blue_sphere"]}/>
           <ViroQuad
@@ -123,34 +142,14 @@ export default class HelloWorldSceneAR extends Component {
             width={.5} height={.5}
             arShadowReceiver={true}
             lightReceivingBitMask={4} />
-        </ViroNode>
+        </ViroNode> */}
       </ViroARScene>
-    );
+    )
   }
 
-  // _onInitialized() {
-  //   var northPoint = this._transformPointToAR(lat_mobile, long_mobile);
-  //   var eastPoint = this._transformPointToAR(lat_mobile, long_mobile);
-  //   var westPoint = this._transformPointToAR(lat_mobile, long_mobile);
-  //   var southPoint = this._transformPointToAR(lat_mobile, long_mobile);
-
-  //   console.log("obj north final x:" + northPoint.x + "final z:" + northPoint.z);
-  //   console.log("obj south final x:" + southPoint.x + "final z:" + southPoint.z);
-  //   console.log("obj east point x" + eastPoint.x + "final z" + eastPoint.z);
-  //   console.log("obj west point x" + westPoint.x + "final z" + westPoint.z);
-
-  //   this.setState({
-  //     northPointX: northPoint.x,
-  //     northPointZ: northPoint.z,
-  //     southPointX: southPoint.x,
-  //     southPointZ: southPoint.z,
-  //     eastPointX: eastPoint.x,
-  //     eastPointZ: eastPoint.z,
-  //     westPointX: westPoint.x,
-  //     westPointZ: westPoint.z,
-  //     text : "AR Init called."
-  //   });
-  // }
+  _onInitialized() {
+    
+  }
 
   _latLongToMerc(lat_deg, lon_deg) {
     var lon_rad = (lon_deg / 180.0 * Math.PI)
@@ -162,29 +161,19 @@ export default class HelloWorldSceneAR extends Component {
   }
 
   _transformPointToAR(lat, long) {
-    const {latMobile, longMobile} = this.state;
-
-    const deviceObjPoint = this.latLongToMerc(lat, long); // see previous post for code.
-    const mobilePoint = this.latLongToMerc(latMobile, longMobile); // see previous post for code.
-
+    const deviceObjPoint = this._latLongToMerc(lat, long); // see previous post for code.
+    const mobilePoint = this._latLongToMerc(this.state.lat_mobile, this.state.long_mobile); // see previous post for code.
     const objDeltaY = deviceObjPoint.y - mobilePoint.y;
     const objDeltaX = deviceObjPoint.x - mobilePoint.x;
-
-    
     let degree = 90; // not using real compass yet.
     let angleRadian = (degree * Math.PI) / 180;
-
-    console.log('Using degree => ', degree);
-    console.log('Angle radian => ', angleRadian);
-
+    // console.log('Using degree => ', degree);
+    // console.log('Angle radian => ', angleRadian);
     let newObjX = objDeltaX * Math.cos(angleRadian) - objDeltaY * Math.sin(angleRadian);
     let newObjY = objDeltaX * Math.sin(angleRadian) + objDeltaY * Math.cos(angleRadian);
-
-    console.log('old delta => ', { x: objDeltaX, z: -objDeltaY });
-    console.log('new delta => ', { x: newObjX, z: -newObjY });
-
-    return { x: newObjX, z: -newObjY };
-
+    // console.log('old delta => ', { x: objDeltaX, z: -objDeltaY });
+    // console.log('new delta => ', { x: newObjX, z: -newObjY });
+    return ({ x: newObjX, z: -newObjY });
   }
 
   _onClick(){
