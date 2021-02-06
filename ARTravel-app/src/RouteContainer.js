4109ac8aa22945fb3818eee1d60734a3b4a87a27
solidw <solidw@naver.com>
2021-02-06 13:10:58 +0900

    a

--- ARTravel-app/src/RouteContainer.js
+++ ARTravel-app/src/RouteContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -12,9 +13,17 @@ import ProfileScreen from './screens/ProfileScreen';
 import AuthScreen from './screens/AuthScreen';
 import PostScreen from './screens/PostScreen';
 import Colors from './styles/Colors';
+import { Image } from 'react-native';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 16,
+    height: 16,
+  },
+});
 
 const PostingStack = () => (
   <Stack.Navigator initialRouteName={routeNames.PICK} headerMode='none'>
@@ -31,14 +40,18 @@ const MainTab = () => (
         let iconName;
 
         if (route.name === routeNames.POSTING) {
-          iconName = !focused ? 'add-circle-outline' : 'add-circle';
+          // iconName = !focused ? 'add-circle-outline' : 'add-circle';
+          return <Image style={styles.icon} source={require('./assets/images/posting.png')} />;
         } else if (route.name === routeNames.FEED) {
-          iconName = !focused ? 'earth' : 'earth-outline';
+          // iconName = !focused ? 'earth' : 'earth-outline';
+          return <Image style={styles.icon} source={require('./assets/images/feed.png')} />;
         } else if (route.name === routeNames.PROFILE) {
-          iconName = !focused ? 'person-circle-outline' : 'person-circle';
+          // iconName = !focused ? 'person-circle-outline' : 'person-circle';
+          // return <Ionicons name={iconName} size={size} color={color} />;
+          return <Image style={styles.icon} source={require('./assets/images/profile.png')} />;
         }
 
-        return <Ionicons name={iconName} size={size} color={color} />;
+        return;
       },
     })}
     tabBarOptions={{
