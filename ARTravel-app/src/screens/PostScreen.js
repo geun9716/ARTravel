f25225a40d65c2f38df64ffd8a848744f78fe0ac
solidw <solidw@naver.com>
2021-02-06 12:10:56 +0900

    gggg

--- ARTravel-app/src/screens/PostScreen.js
+++ ARTravel-app/src/screens/PostScreen.js
@@ -1,20 +1,13 @@
-import React, { useState, useEffect } from "react";
-import {
-  View,
-  ScrollView,
-  Text,
-  StyleSheet,
-  Image,
-  Dimensions,
-  TextInput,
-} from "react-native";
-import Icon from "react-native-vector-icons/Ionicons";
+import React, { useState, useEffect } from 'react';
+import { View, ScrollView, Text, StyleSheet, Image, Dimensions, TextInput } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-import Screen from "../components/Screen";
-import Header from "../components/Header";
-import Colors from "../styles/Colors";
+import ApiClient from '../modules/ApiClient';
+import Screen from '../components/Screen';
+import Header from '../components/Header';
+import Colors from '../styles/Colors';
 
-const { width } = Dimensions.get("window");
+const { width } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
@@ -35,72 +28,51 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     height: 40,
     borderRadius: 10,
-    borderColor: "gray",
+    borderColor: 'gray',
     borderWidth: 1,
   },
 });
 
-let selectImage;
-const ProfileScreen = (route) => {
-  // const [imageList, setImageList] = useState([]);
-  selectImage = route.params.selectImage;
-  console.log(route.params);
+const PostScreen = ({ route }) => {
+  const [postInfo, setPostInfo] = useState([]);
+  const { postID } = route.params;
+
   useEffect(() => {
-    // const fetchData = async () => {
-    //   try {
-    //     const { data } = await instance.get(`/user/profile/${userId}`);
-    //     console.log(data.userPost[0].imgPath);
-    //     for (i in data.userPost) {
-    //       postList.push(data.userPost[i].imgPath);
-    //     }
-    //     setImageList(postList);
-    //     setIsLoading(false);
-    //   } catch (err) {
-    //     console.error(err);
-    //   }
-    // };
-    // fetchData();
+    const fetchData = async () => {
+      try {
+        const { data } = await ApiClient.get(`/post/${postID}`);
+        setPostInfo(data.postInfo[0]);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchData();
   }, []); // 1회만 실행함을 의미
 
   return (
     <Screen style={styles.container}>
-      <Header
-        title={"포스트"}
-        left={
-          <Icon
-            onPress={() => setIsClicked(false)}
-            style={styles.headerButton}
-            name="close"
-            size={20}
-          />
-        }
-      />
-      <View style={{ flexDirection: "row", paddingTop: 10 }}>
-        <View style={{ flex: 1, alignItems: "center" }}>
-          <Image
-            source={{ uri: "https://steemitimages.com/u/anpigon/avatar" }}
-            style={{ width: 75, height: 75, borderRadius: 37.5 }}
-          />
+      <Header title={'포스트'} />
+      <View style={{ flexDirection: 'row', paddingTop: 10 }}>
+        <View style={{ flex: 1, alignItems: 'center' }}>
+          <Image source={{ uri: 'https://steemitimages.com/u/anpigon/avatar' }} style={{ width: 75, height: 75, borderRadius: 37.5 }} />
         </View>
         <View style={{ flex: 3 }}>
-          <View style={{ alignItems: "center" }}>
-            <Text style={{ color: "black" }}>홍길동</Text>
-            <Text style={{ fontSize: 10, color: "gray" }}>Traveler</Text>
+          <View style={{ alignItems: 'center' }}>
+            <Text style={{ color: 'black' }}>홍길동</Text>
+            <Text style={{ fontSize: 10, color: 'gray' }}>Traveler</Text>
           </View>
-          <View
-            style={{ flexDirection: "row", justifyContent: "space-around" }}
-          >
-            <View style={{ alignItems: "center" }}>
-              <Text style={{ color: "black" }}>50</Text>
-              <Text style={{ fontSize: 10, color: "gray" }}>남긴 AR</Text>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+            <View style={{ alignItems: 'center' }}>
+              <Text style={{ color: 'black' }}>50</Text>
+              <Text style={{ fontSize: 10, color: 'gray' }}>남긴 AR</Text>
             </View>
-            <View style={{ alignItems: "center" }}>
-              <Text style={{ color: "black" }}>336</Text>
-              <Text style={{ fontSize: 10, color: "gray" }}>받은 하트</Text>
+            <View style={{ alignItems: 'center' }}>
+              <Text style={{ color: 'black' }}>336</Text>
+              <Text style={{ fontSize: 10, color: 'gray' }}>받은 하트</Text>
             </View>
-            <View style={{ alignItems: "center" }}>
-              <Text style={{ color: "black" }}>1061</Text>
-              <Text style={{ fontSize: 10, color: "gray" }}>보낸 하트</Text>
+            <View style={{ alignItems: 'center' }}>
+              <Text style={{ color: 'black' }}>1061</Text>
+              <Text style={{ fontSize: 10, color: 'gray' }}>보낸 하트</Text>
             </View>
           </View>
         </View>
@@ -109,49 +81,49 @@ const ProfileScreen = (route) => {
         <View style={{ width: width, height: width }}>
           <Image
             source={{
-              uri: selectImage,
+              uri: postInfo.imgPath,
             }}
             style={{ flex: 1 }}
           />
         </View>
-        <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+        <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
           <Text>하트</Text>
-          <Text> 10 likes</Text>
+          <Text>{postInfo.likeCount}</Text>
         </View>
         <Text style={{ marginBottom: 10 }}>인생샷</Text>
-        <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
-          <Text style={{ fontWeight: "bold", color: "black" }}>김철수</Text>
+        <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+          <Text style={{ fontWeight: 'bold', color: 'black' }}>김철수</Text>
           <Text> 좋아~</Text>
         </View>
-        <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
-          <Text style={{ fontWeight: "bold", color: "black" }}>김영희</Text>
+        <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+          <Text style={{ fontWeight: 'bold', color: 'black' }}>김영희</Text>
           <Text> 이쁘네 ㅎㅎ</Text>
         </View>
-        <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
-          <Text style={{ fontWeight: "bold", color: "black" }}>김철수</Text>
+        <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+          <Text style={{ fontWeight: 'bold', color: 'black' }}>김철수</Text>
           <Text> 좋아~</Text>
         </View>
-        <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
-          <Text style={{ fontWeight: "bold", color: "black" }}>김영희</Text>
+        <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+          <Text style={{ fontWeight: 'bold', color: 'black' }}>김영희</Text>
           <Text> 이쁘네 ㅎㅎ</Text>
         </View>
-        <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
-          <Text style={{ fontWeight: "bold", color: "black" }}>김철수</Text>
+        <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+          <Text style={{ fontWeight: 'bold', color: 'black' }}>김철수</Text>
           <Text> 좋아~</Text>
         </View>
-        <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
-          <Text style={{ fontWeight: "bold", color: "black" }}>김영희</Text>
+        <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+          <Text style={{ fontWeight: 'bold', color: 'black' }}>김영희</Text>
           <Text> 이쁘네 ㅎㅎ</Text>
         </View>
         <TextInput
           style={styles.textInput}
-          placeholder="댓글을 작성해주세요..."
+          placeholder='댓글을 작성해주세요...'
           // backgroundColor="gray"
-          selectionColor="gray"
-        ></TextInput>
+          selectionColor='gray'
+        />
       </ScrollView>
     </Screen>
   );
 };
 
-export default ProfileScreen;
+export default PostScreen;
--- ARTravel-app/src/screens/ProfileScreen.js
+++ ARTravel-app/src/screens/ProfileScreen.js
@@ -1,25 +1,15 @@
-import React, { useState, useEffect } from "react";
-import {
-  View,
-  ScrollView,
-  Text,
-  StyleSheet,
-  Image,
-  Dimensions,
-  TouchableOpacity,
-  TextInput,
-} from "react-native";
-import Icon from "react-native-vector-icons/Ionicons";
-import { useNavigation } from "@react-navigation/native";
+import React, { useState, useEffect } from 'react';
+import { View, ScrollView, Text, StyleSheet, Image, Dimensions, TouchableOpacity, TextInput } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-import Screen from "../components/Screen";
-import LoadingIndicator from "../components/LoadingIndicator";
-import instance from "../modules/ApiClient";
-import Header from "../components/Header";
-import Colors from "../styles/Colors";
-import { routeNames } from "../constants";
+import Screen from '../components/Screen';
+import LoadingIndicator from '../components/LoadingIndicator';
+import instance from '../modules/ApiClient';
+import Header from '../components/Header';
+import Colors from '../styles/Colors';
+import { routeNames } from '../constants';
 
-const { width } = Dimensions.get("window");
+const { width } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
@@ -40,25 +30,26 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     height: 40,
     borderRadius: 10,
-    borderColor: "gray",
+    borderColor: 'gray',
     borderWidth: 1,
   },
 });
 
-const postList = [];
 const ProfileScreen = () => {
-  const [imageList, setImageList] = useState([]);
   const userId = 1;
+  const navigation = useNavigation();
 
+  const [imageList, setImageList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        const postList = [];
         const { data } = await instance.get(`/user/profile/${userId}`);
-        console.log(data.userPost[0].imgPath);
+        console.log(data);
         for (i in data.userPost) {
-          postList.push(data.userPost[i].imgPath);
+          postList.push(data.userPost[i]);
         }
         setImageList(postList);
         setIsLoading(false);
@@ -71,54 +62,49 @@ const ProfileScreen = () => {
 
   return (
     <Screen style={styles.container}>
-      <Header title={"내 피드"} />
-      <View style={{ flexDirection: "row", paddingTop: 10 }}>
-        <View style={{ flex: 1, alignItems: "center" }}>
-          <Image
-            source={{ uri: "https://steemitimages.com/u/anpigon/avatar" }}
-            style={{ width: 75, height: 75, borderRadius: 37.5 }}
-          />
+      <Header title={'내 피드'} />
+      <View style={{ flexDirection: 'row', paddingTop: 10 }}>
+        <View style={{ flex: 1, alignItems: 'center' }}>
+          <Image source={{ uri: 'https://steemitimages.com/u/anpigon/avatar' }} style={{ width: 75, height: 75, borderRadius: 37.5 }} />
         </View>
         <View style={{ flex: 3 }}>
-          <View style={{ alignItems: "center" }}>
-            <Text style={{ color: "black" }}>홍길동</Text>
-            <Text style={{ fontSize: 10, color: "gray" }}>Traveler</Text>
+          <View style={{ alignItems: 'center' }}>
+            <Text style={{ color: 'black' }}>홍길동</Text>
+            <Text style={{ fontSize: 10, color: 'gray' }}>Traveler</Text>
           </View>
-          <View
-            style={{ flexDirection: "row", justifyContent: "space-around" }}
-          >
-            <View style={{ alignItems: "center" }}>
-              <Text style={{ color: "black" }}>50</Text>
-              <Text style={{ fontSize: 10, color: "gray" }}>남긴 AR</Text>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+            <View style={{ alignItems: 'center' }}>
+              <Text style={{ color: 'black' }}>50</Text>
+              <Text style={{ fontSize: 10, color: 'gray' }}>남긴 AR</Text>
             </View>
-            <View style={{ alignItems: "center" }}>
-              <Text style={{ color: "black" }}>336</Text>
-              <Text style={{ fontSize: 10, color: "gray" }}>받은 하트</Text>
+            <View style={{ alignItems: 'center' }}>
+              <Text style={{ color: 'black' }}>336</Text>
+              <Text style={{ fontSize: 10, color: 'gray' }}>받은 하트</Text>
             </View>
-            <View style={{ alignItems: "center" }}>
-              <Text style={{ color: "black" }}>1061</Text>
-              <Text style={{ fontSize: 10, color: "gray" }}>보낸 하트</Text>
+            <View style={{ alignItems: 'center' }}>
+              <Text style={{ color: 'black' }}>1061</Text>
+              <Text style={{ fontSize: 10, color: 'gray' }}>보낸 하트</Text>
             </View>
           </View>
         </View>
       </View>
       <ScrollView>
-        <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+        <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
           {isLoading ? (
             <LoadingIndicator />
           ) : (
-            postList.map((image, index) => {
+            imageList.map((image, index) => {
               return (
                 <TouchableOpacity
                   onPress={() =>
                     navigation.navigate(routeNames.POST, {
-                      selectImage: image,
+                      postID: image.postID,
                     })
                   }
                   key={index}
                   style={{ width: width / 3, height: width / 3 }}
                 >
-                  <Image source={{ uri: image }} style={{ flex: 1 }} />
+                  <Image source={{ uri: image.imgPath }} style={{ flex: 1 }} />
                 </TouchableOpacity>
               );
             })
